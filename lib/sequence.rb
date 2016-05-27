b1107ae6bb8b0eff7e63cfd107e7316bd87c8753
Artjom Juferov <derevjaginti@gmail.com>
2016-05-27 15:31:56 +0200

    Sequence class with default tests for #sort

--- lib/sequence.rb
+++ lib/sequence.rb
@@ -1,14 +1,60 @@
 require_relative 'job'
 
 class Sequence
+
   #pre_jobs stores just names not object
   def initialize pre_jobs
     #creating graph from pre_jobs
     @jobs = make_jobs pre_jobs
+    # validate on circular
+    validate_on_circular
+  end
+
+  def sort
+    @jobs.each_with_index do |job, ind|
+      # if job has ticket it was marked
+      next if job.ticket
+      sort_dfs job, ind*@jobs.size
+    end
+    # now every job has their own right ticket
+    @jobs = @jobs.sort_by(&:ticket).reverse
+  end
+
+  def to_s
+    @jobs.inject(''){|result, job| result += job.name.to_s }
+  end
+
+  private
+
+  def validate_on_circular
+    @jobs.each do |job|
+      next if job.ticket
+      validate_dfs job
+    end
+  end
+
+  def validate_dfs job
+    return unless job
+    # if we during this dfs have visited this edge it's circular
+    raise CircularDepError if job.ticket
+    job.ticket = true
+    validate_dfs job.dep_job
+    # set nil after ending dfs session
+    job.ticket = nil
   end
 
 
-  private
+  def sort_dfs job, ticket
+    return unless job
+
+    # if it's depended on job it has to have previous ticket
+    if job.dep_job and job.dep_job.ticket
+        job.ticket = job.dep_job.ticket-2
+    end
+    job.ticket = ticket
+    sort_dfs job.dep_job, ticket+1
+  end
+
 
   def make_jobs pre_jobs
     # create jobs without dependencies
@@ -26,5 +72,11 @@ class Sequence
 
   def find_job jobs, name
     jobs.find{ |job| job.name == name }
+  end
+end
+
+class CircularDepError < StandardError
+  def initialize
+    super "There is circular dependencies"
   end
 end
--- spec/sequence_spec.rb
+++ spec/sequence_spec.rb
@@ -3,12 +3,14 @@ require './lib/pre_job.rb'
 
 RSpec.describe Sequence do
 
+  let(:pre_jobs) { [PreJob.new('a')]}
+
   context '#make_jobs' do
     subject { Sequence.new(pre_jobs).send(:make_jobs, pre_jobs) }
 
-    context 'when everything is correct' do
-      let(:pre_jobs) { [PreJob.new('a', 'b'), PreJob.new('b')]}
+    let(:pre_jobs) { [PreJob.new('a', 'b'), PreJob.new('b')]}
 
+    context 'when everything is correct' do
       it { is_expected.to be_an Array}
 
       it 'returns array of jobs' do
@@ -28,5 +30,59 @@ RSpec.describe Sequence do
         expect(job_a.dep_job.name).to eq 'b'
       end
     end
+  end
+
+  context "#to_s" do
+    subject { Sequence.new(pre_jobs).to_s }
+
+    it { is_expected.to eq 'a' }
+  end
+
+  context '#sort' do
+    context 'when without errors' do
+      inputs = [
+          %w(a),
+          %w(a b c),
+          %w(a bc c),
+          %w(a bc cf da eb f)
+      ]
+
+      inputs.each do |input|
+        context "when input = #{input}" do
+          it 'returns right value' do
+            pre_jobs = make_pre_jobs input
+            seq = Sequence.new pre_jobs
+            seq.sort
+            # for checkin select only dependencies
+            dep_inputs = input.select{|x| x.size == 2}
+            dep_inputs.each do |jobs|
+              job_ind = seq.to_s.index jobs[0]
+              dep_ind = seq.to_s.index jobs[1]
+              # job index has to be after
+              expect(job_ind).to be > dep_ind
+            end
+          end
+        end
+      end
+    end
+
+    context 'when there is circular error' do
+      in_out = [
+          %w(a bc cf da e fb)
+      ]
+
+      in_out.each do |input|
+        it 'raises error' do
+          expect{ Sequence.new make_pre_jobs(input) }.to raise_error CircularDepError
+        end
+      end
+    end
+  end
+
+
+  private
+
+  def make_pre_jobs input
+    input.map {|chars| PreJob.new chars[0], chars[1] }
   end
 end
